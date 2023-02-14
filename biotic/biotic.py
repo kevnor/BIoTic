a97503fc91b14408d4de74eb543f1c05ba36be35
Kevin Nordnes <kevinnordnes@proton.me>
2023-02-14 14:52:51 +0100

    hei

--- biotic/biotic.py
+++ biotic/biotic.py
@@ -5,11 +5,12 @@ xbee = XBeeDevice("/dev/tty16", 9600)
 
 # Get the XBee network object from the local XBee.
 xnet = xbee.get_network()
+print(xnet)
 
 # Start the discovery process and wait for it to be over.
-xnet.start_discovery_process()
-while xnet.is_discovery_running():
-    time.sleep(0.5)
+#xnet.start_discovery_process()
+#while xnet.is_discovery_running():
+#    time.sleep(0.5)
 
 # Get the list of the nodes in the network.
-nodes = xnet.get_devices()
+#nodes = xnet.get_devices()
